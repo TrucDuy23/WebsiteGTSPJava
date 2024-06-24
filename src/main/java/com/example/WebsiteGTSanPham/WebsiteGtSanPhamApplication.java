eb1e2e6cee8b7d70d48d11e4b33adfe88f6f0e43
TrucDuy <TrucDuy@LAPTOP-3K2TB8FV>
2024-06-24 21:26:34 +0700

    Master

---
+++ src/main/java/com/example/WebsiteGTSanPham/WebsiteGtSanPhamApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteGTSanPham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteGtSanPhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteGtSanPhamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebsiteGTSanPham/WebsiteGtSanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteGTSanPham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteGtSanPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
