eb41d1e31b334458627139b75125a9e947dce47f
Đức Mạnh Trần <manht260@gmail.com>
2024-06-28 23:34:11 +0700

    chức năng đăng nhập đăng ký phân quyền

---
+++ src/main/java/com/example/WebsiteGTSanPham/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.WebsiteGTSanPham.repository;
+
+import com.example.WebsiteGTSanPham.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
